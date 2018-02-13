63576d21c395c030da53298876064912e0235d0b
philominraj <32054320+philominraj@users.noreply.github.com>
2018-02-13 19:05:21 +0530

    Add files via upload
    First upload

---
+++ src/main/java/com/omsauth0/jwt/springboot/security/OmsAuth0JwtSpringbootSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.omsauth0.jwt.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsAuth0JwtSpringbootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsAuth0JwtSpringbootSecurityApplication.class, args);
+	}
+}
